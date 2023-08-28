50fef6266a826c4629f552cbe3b7cbe1f3e8b800
cAos <65823540+Cesar-580@users.noreply.github.com>
2023-08-28 16:22:13 -0500

    Se cargo el codigo obtenido por el chatGPT

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+# Título y autor de la app
+st.title("Mi primera app")
+st.write("Esta app fue elaborada por “Cesar Augusto Ospina Muñoz”.")
+
+# Preguntar el nombre al usuario
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
+
+# Verificar si se ingresó un nombre
+if nombre_usuario:
+    # Mostrar el mensaje de bienvenida
+    mensaje_bienvenida = f"{nombre_usuario}, te doy la bienvenida a mi primera app."
+    st.write(mensaje_bienvenida)
